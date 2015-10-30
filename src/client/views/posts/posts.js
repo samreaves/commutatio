4126b9d819c5eaedb402a6ad463d0f5f06a38a67
Sam <thesamreaves@gmail.com>
2015-10-30 17:49:35 -0400

    Added server to serve static content. Refactored client side npm modules to bower modules so that client could access them.

---
+++ src/client/views/posts/posts.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('commutatio.posts', ['ngRoute'])
+
+// Configure root to use the posts partial and PostsCtrl controller
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'views/posts/posts.html',
+    controller: 'PostsCtrl'
+  });
+}])
+
+// Set up EncryptionCtrl controller
+.controller("PostsCtrl", function($scope) {
+
+  $scope.posts = [
+    {
+      author: "sreaves",
+      category: "soccer",
+      content: "barcelona is the best. they have a vampire with a messi haircut",
+      datetime: "October 30, 2015 12:35pm",
+      tags: ["soccer", "fcbarcelona"],
+      title: "barcelona kicks ass",
+      votes: 1
+    },
+    {
+      author: "mlomas",
+      category: "humans",
+      content: "humans that become clowns can't fly",
+      datetime: "October 30, 2015 12:55pm",
+      tags: ["humans", "clowns"],
+      title: "clowns don't fly",
+      votes: 1
+    }
+  ]
+
+});
---
+++ src/server/app.js
@@ -0,0 +1,43 @@
+/**
+ * @name Commutatio Server
+ * @file app.js
+ * @description Initializes server. 
+ * @author Sam Reaves
+ * @date October 30th, 2015
+ */
+
+// Import Express and initialize server.
+var express = require('express'),
+	app = module.exports.app = exports.app = express(),
+	logger = require('morgan'),
+	path = require('path'),
+	body_parser = require('body-parser'),
+	server,
+	host,
+	port,
+	static_directory = path.join(__dirname, '../client/');
+
+// Use body parser to parse both application/json and application/x-www-form-urlencoded
+app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.json());
+
+// Use Morgan logger
+app.use(logger('dev'));
+
+// Establish static directory at client/public
+app.use(express.static(static_directory));
+
+// Route handler for root - sends current static landing page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(static_directory, "index.html"));
+});
+
+// Server starts listening on port 3000.
+server = app.listen(8000, function() {
+
+	host = server.address().address,
+  	port = server.address().port;
+
+  	// Logs a message to let dev know we're up and running.
+  	console.log('Example app listening at http://%s:%s', host, port);
+});
