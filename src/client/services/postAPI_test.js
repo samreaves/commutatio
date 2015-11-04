9703e0dab256899e09311bf4ff16dac4cc2cd64e
Sam <thesamreaves@gmail.com>
2015-11-03 22:05:59 -0500

    Added in test for getAllPosts

--- src/client/services/postAPI_test.js
+++ src/client/services/postAPI_test.js
@@ -76,7 +76,7 @@ describe('factory: postAPI', function() {
   	})
   })
 
-  // Make sure getAllValuesSortedByCategory method works as planned
+  // Make sure getPostById method works as planned
   describe('method postAPI.getPostById toEqual', function() {
   	it('should equal', function(){
 		  // simulate scope variable
@@ -104,5 +104,47 @@ describe('factory: postAPI', function() {
                       votes: 1
                     })
   	})
+  })
+
+  // Make sure getAllPosts method works as planned
+  describe('method postAPI.getAllPosts toEqual', function() {
+    it('should equal', function(){
+      // simulate scope variable
+      var posts;
+      
+      // Make the AJAX call
+      postAPI.getAllPosts().then(function(data) {
+        
+        // Simulate the DOM updating
+        posts = data;
+      })
+      
+      // Flush timeouts so that the promise resolves
+      timeout.flush();
+
+      // Expect the DOM's value to have updated
+      expect(posts).toEqual([
+                    {
+                      author: "sreaves",
+                      category: "soccer",
+                      content: "barcelona is the best. they have a vampire with a messi haircut",
+                      datetime: "October 30, 2015 12:35pm",
+                      id: 1,
+                      tags: ["soccer", "fcbarcelona"],
+                      title: "barcelona kicks ass",
+                      votes: 1
+                    },
+                    {
+                      author: "mlomas",
+                      category: "humans",
+                      content: "humans that become clowns can't fly",
+                      datetime: "October 30, 2015 12:55pm",
+                      id: 2,
+                      tags: ["humans", "clowns"],
+                      title: "clowns don't fly",
+                      votes: 1
+                    }
+                  ])
+    })
   })
 });
